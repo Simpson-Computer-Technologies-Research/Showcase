ad8109c72013bc09895e58558795fba3805236d7
Tristan Simpson <heytristaann@gmail.com>
2022-11-15 21:23:44 -0500

    Updates

---
+++ _app/immutable/chunks/0-584f8487.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-2ee7b3f9.js";export{t as component};
---
+++ _app/immutable/chunks/1-eeaed237.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-808a0e99.js";export{t as component};
